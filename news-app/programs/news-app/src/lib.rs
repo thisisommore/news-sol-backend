deb6d76e20eaff50349118412c5cc6aeb7e72f6a
thisisommore <ommore501@gmail.com>
2023-03-22 03:31:30 +0000

    Create basic news program

--- news-app/programs/news-app/src/lib.rs
+++ news-app/programs/news-app/src/lib.rs
@@ -3,13 +3,77 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod news_app {
+pub mod news {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_news(
+        ctx: Context<CreateNews>,
+        title: String,
+        description: String,
+        place: String,
+        image: String,
+        category: String,
+        date: i64,
+        video_link: String,
+        keywords: Vec<String>,
+    ) -> ProgramResult {
+        let news_account = &mut ctx.accounts.news;
+        news_account.title = title;
+        news_account.description = description;
+        news_account.place = place;
+        news_account.image = image;
+        news_account.category = category;
+        news_account.date = date;
+        news_account.video_link = video_link;
+        news_account.keywords = keywords;
+        news_account.creator = *ctx.accounts.creator.to_account_info().key;
+        Ok(())
+    }
+
+    pub fn update_views(ctx: Context<UpdateViews>, views: u64) -> ProgramResult {
+        let news_account = &mut ctx.accounts.news;
+        if *ctx.accounts.creator.to_account_info().key != news_account.creator {
+            return Err(ErrorCode::Unauthorized.into());
+        }
+        news_account.views = views;
         Ok(())
     }
 }
 
+#[account]
+pub struct News {
+    pub title: String,
+    pub description: String,
+    pub place: String,
+    pub image: String,
+    pub category: String,
+    pub views: u64,
+    pub date: i64,
+    pub video_link: String,
+    pub keywords: Vec<String>,
+    pub creator: Pubkey,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateNews<'info> {
+    #[account(init, payer = creator, space = 8 + 32 + 32 + 32 + 32 + 8 + 64 + 8 + 8 + 8 + 32)]
+    pub news: Account<'info, News>,
+    #[account(signer)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateViews<'info> {
+    #[account(mut)]
+    pub news: Account<'info, News>,
+    #[account(signer)]
+    pub creator: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Unauthorized")]
+    Unauthorized,
+}
