4cb6911b38cbd0b901087ed71dc5a0d41b3adbcb
thisisommore <ommore501@gmail.com>
2023-04-08 05:04:51 +0000

    Update program to improve filtering by fixing field size

--- news/programs/news/src/lib.rs
+++ news/programs/news/src/lib.rs
@@ -1,6 +1,14 @@
 use anchor_lang::prelude::*;
 
-declare_id!("3hJc8UD8dCKpuJiBzKwfeAmxBS2AmCCbUczKRL7x3QGV");
+declare_id!("4qf7wofgoJz27cx3e8j5MqKqEdnMyCFtXexzPh16GCnt");
+
+const TITLE_LEN: usize = 32;
+const DESCRIPTION_LEN: usize = 64;
+const PLACE_LEN: usize = 32;
+const IMAGE_LEN: usize = 32;
+const CATEGORY_LEN: usize = 32;
+const VIDEO_LINK_LEN: usize = 64;
+const KEYWORDS_LEN: usize = 8;
 
 #[program]
 pub mod news {
@@ -8,14 +16,15 @@ pub mod news {
 
     pub fn create_news(
         ctx: Context<CreateNews>,
-        title: String,
-        description: String,
-        place: String,
-        image: String,
-        category: String,
-        date: i64,
-        video_link: String,
-        keywords: Vec<String>,
+        title: [u8; TITLE_LEN],
+        description: [u8; DESCRIPTION_LEN],
+        place: [u8; PLACE_LEN],
+        image: [u8; IMAGE_LEN],
+        category: [u8; CATEGORY_LEN],
+        month: u8,
+        year: u16,
+        video_link: [u8; VIDEO_LINK_LEN],
+        keywords: [[u8; KEYWORDS_LEN]; 5],
     ) -> Result<()> {
         let news_account = &mut ctx.accounts.news;
         news_account.title = title;
@@ -23,7 +32,8 @@ pub mod news {
         news_account.place = place;
         news_account.image = image;
         news_account.category = category;
-        news_account.date = date;
+        news_account.month = month;
+        news_account.year = year;
         news_account.video_link = video_link;
         news_account.keywords = keywords;
         news_account.creator = *ctx.accounts.creator.to_account_info().key;
@@ -42,21 +52,22 @@ pub mod news {
 
 #[account]
 pub struct News {
-    pub title: String,
-    pub description: String,
-    pub place: String,
-    pub image: String,
-    pub category: String,
+    pub title: [u8; TITLE_LEN],
+    pub description: [u8; DESCRIPTION_LEN],
+    pub place: [u8; PLACE_LEN],
+    pub image: [u8; IMAGE_LEN],
+    pub category: [u8; CATEGORY_LEN],
     pub views: u64,
-    pub date: i64,
-    pub video_link: String,
-    pub keywords: Vec<String>,
+    pub month: u8,
+    pub year: u16,
+    pub video_link: [u8; VIDEO_LINK_LEN],
+    pub keywords: [[u8; KEYWORDS_LEN]; 5],
     pub creator: Pubkey,
 }
 
 #[derive(Accounts)]
 pub struct CreateNews<'info> {
-    #[account(init, payer = creator, space = 8 + 32 + 32 + 32 + 32 + 8 + 64 + 8 + 8 + 8 + 32)]
+    #[account(init, payer = creator, space = 8 + TITLE_LEN + DESCRIPTION_LEN + PLACE_LEN + IMAGE_LEN + CATEGORY_LEN + 8 + 8 + VIDEO_LINK_LEN + 5 * KEYWORDS_LEN + 32)]
     pub news: Account<'info, News>,
     #[account(mut)]
     pub creator: Signer<'info>,
--- news/tests/news.ts
+++ news/tests/news.ts
@@ -3,6 +3,27 @@ import { Program } from "@coral-xyz/anchor";
 import { BN } from "bn.js";
 import { assert } from "chai";
 import { News } from "../target/types/news";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+const DISC = 8;
+const MONTH_LEN = 1
+const VIEWS_LEN = 8
+const TITLE_LEN = 32;
+const DESCRIPTION_LEN = 64;
+const PLACE_LEN = 32;
+const IMAGE_LEN = 32;
+const CATEGORY_LEN = 32;
+const VIDEO_LINK_LEN = 64;
+const KEYWORDS_LEN = 8;
+
+function stringToBuffer(str, len) {
+  let buf = Buffer.alloc(len);
+  buf.write(str);
+  return Array.from(buf.slice(0, len));
+}
+
+function arrayToString(stren) {
+  return Buffer.from(stren).toString().replace(/\x00+$/, '');
+}
 
 describe("news-app", () => {
   const provider = anchor.AnchorProvider.env()
@@ -18,20 +39,22 @@ describe("news-app", () => {
     const place = "Test News Place";
     const image = "Test News Image URL";
     const category = "Test News Category";
-    const date = new BN(new Date().getTime());
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
     const video_link = "Test News Video Link";
-    const keywords = ["Test", "News", "Keywords"];
+    const keywords = ["Test", "News", "Keywords", "WER", "erew"].map(keyword => stringToBuffer(keyword, KEYWORDS_LEN));
 
     const news_account = anchor.web3.Keypair.generate()
     // Act
     const tx = await program.methods.createNews(
-      title,
-      description,
-      place,
-      image,
-      category,
-      date,
-      video_link,
+      stringToBuffer(title, TITLE_LEN),
+      stringToBuffer(description, DESCRIPTION_LEN),
+      stringToBuffer(place, PLACE_LEN),
+      stringToBuffer(image, IMAGE_LEN),
+      stringToBuffer(category, CATEGORY_LEN),
+      month,
+      year,
+      stringToBuffer(video_link, VIDEO_LINK_LEN),
       keywords,
     ).accounts({
       creator: creator.publicKey,
@@ -41,17 +64,15 @@ describe("news-app", () => {
 
     const fetched_news_account = await program.account.news.fetch(news_account.publicKey)
 
-    assert.ok(fetched_news_account.creator.equals(creator.publicKey));
-    assert.ok(fetched_news_account.title === title);
-    assert.ok(fetched_news_account.description === description);
-    assert.ok(fetched_news_account.place === place);
-    assert.ok(fetched_news_account.image === image);
-    assert.ok(fetched_news_account.category === category);
-    assert.ok(fetched_news_account.date.eq(date));
-    assert.ok(fetched_news_account.videoLink === video_link);
-    assert.ok(fetched_news_account.keywords[0] === keywords[0]);
-    assert.ok(fetched_news_account.keywords[1] === keywords[1]);
-    assert.ok(fetched_news_account.keywords[2] === keywords[2]);
+    assert.equal(fetched_news_account.creator.toString(), creator.publicKey.toString());
+    assert.equal(arrayToString(fetched_news_account.title), title);
+    assert.equal(arrayToString(fetched_news_account.description), description);
+    assert.equal(arrayToString(fetched_news_account.place), place);
+    assert.equal(arrayToString(fetched_news_account.image), image);
+    assert.equal(arrayToString(fetched_news_account.category), category);
+    assert.equal(fetched_news_account.month, month);
+    assert.equal(arrayToString(fetched_news_account.videoLink), video_link);
+    assert.deepEqual(fetched_news_account.keywords, keywords);
   });
 
   it("updates the views of a news article", async () => {
@@ -65,19 +86,22 @@ describe("news-app", () => {
     const category = "Test News Category";
     const date = new Date().getTime();
     const video_link = "Test News Video Link";
-    const keywords = ["Test", "News", "Keywords"];
+    const keywords = ["Test", "News", "Keywords", "", ""].map(keyword => stringToBuffer(keyword, KEYWORDS_LEN));
     const views = 10;
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
 
     const news_account = anchor.web3.Keypair.generate()
     // Act
     const createTx = await program.methods.createNews(
-      title,
-      description,
-      place,
-      image,
-      category,
-      new BN(date),
-      video_link,
+      stringToBuffer(title, TITLE_LEN),
+      stringToBuffer(description, DESCRIPTION_LEN),
+      stringToBuffer(place, PLACE_LEN),
+      stringToBuffer(image, IMAGE_LEN),
+      stringToBuffer(category, CATEGORY_LEN),
+      month,
+      year,
+      stringToBuffer(video_link, VIDEO_LINK_LEN),
       keywords,
     ).accounts({
       creator: creator.publicKey,
@@ -101,6 +125,76 @@ describe("news-app", () => {
 
     assert.ok(fetched_news_account_after_update.views.eq(new BN(views)));
 
-  }
-  )
+  })
+
+  it("it fetched by title", async () => {
+    // Arrange
+    const creator = provider.wallet;
+    const title = "I know how to study but wowo";
+    const description = "Test News Description";
+    const place = "Test News Place";
+    const image = "Test News Image URL";
+    const category = "Test News Category";
+    const video_link = "Test News Video Link";
+    const keywords = ["Test", "News", "Keywords", "", ""].map(keyword => stringToBuffer(keyword, KEYWORDS_LEN));;
+    const month = new Date().getMonth();
+
+    const year = new Date().getFullYear();
+    const news_account = anchor.web3.Keypair.generate()
+    // Act
+    const tx = await program.methods.createNews(
+      stringToBuffer(title, TITLE_LEN),
+      stringToBuffer(description, DESCRIPTION_LEN),
+      stringToBuffer(place, PLACE_LEN),
+      stringToBuffer(image, IMAGE_LEN),
+      stringToBuffer(category, CATEGORY_LEN),
+      month,
+      year,
+      stringToBuffer(video_link, VIDEO_LINK_LEN),
+      keywords,
+    ).accounts({
+      creator: creator.publicKey,
+      news: news_account.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).signers([news_account]).rpc();
+
+
+    // const max = await provider.connection.getProgramAccounts(program.programId)
+    // const start_off_set = DISC + TITLE_LEN + DESCRIPTION_LEN + PLACE_LEN + IMAGE_LEN + CATEGORY_LEN + VIEWS_LEN
+    // const maxes = max.map(e => e.account.data.slice(start_off_set, start_off_set + 1).toString("hex"))
+
+    const fetched_news_accounts = await program.account.news.all([
+      {
+        memcmp: {
+          offset: 8,
+          bytes: bs58.encode(Buffer.from(title))
+        },
+      },
+      {
+        memcmp: {
+          offset: 8 + TITLE_LEN,
+          bytes: bs58.encode(Buffer.from(description))
+        },
+      },
+      {
+        memcmp: {
+          offset: DISC + TITLE_LEN + DESCRIPTION_LEN + PLACE_LEN + IMAGE_LEN + CATEGORY_LEN + VIEWS_LEN,
+          bytes: bs58.encode(Buffer.from([month]))
+        },
+      },
+    ])
+
+    const fetched_news_account = fetched_news_accounts[0].account
+
+    assert.equal(fetched_news_account.creator.toString(), creator.publicKey.toString());
+    assert.equal(arrayToString(fetched_news_account.title), title);
+    assert.equal(arrayToString(fetched_news_account.description), description);
+    assert.equal(arrayToString(fetched_news_account.place), place);
+    assert.equal(arrayToString(fetched_news_account.image), image);
+    assert.equal(arrayToString(fetched_news_account.category), category);
+    assert.equal(fetched_news_account.month, month);
+    assert.equal(arrayToString(fetched_news_account.videoLink), video_link);
+    assert.deepEqual(fetched_news_account.keywords, keywords);
+
+  })
 })
